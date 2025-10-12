f75723fbd0755533fc9efe0fc8032044e687ec02
¨susy215¨ <yandiravivancos@gmail.com>
2025-10-11 23:34:41 -0400

    fix cambios mejoras

--- cooperativa_backend/cooperativa/models.py
+++ cooperativa_backend/cooperativa/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.db.models import Q
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 from django.utils import timezone
 from django.core.exceptions import ValidationError
